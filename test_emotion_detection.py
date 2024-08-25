cb4614c348f5f0b8a020e47dfdc17f60f13cbc16
Eduard Mutafyan <eduard.mutafyan@ioio.tv>
2024-08-25 14:40:20 +0300

    Implemented emotion detection

---
+++ test_emotion_detection.py
@@ -0,0 +1,26 @@
+from EmotionDetection.emotion_detection import emotion_detector
+import unittest
+
+DOMINANT = "dominant_emotion"
+
+
+class TestEmotionDetector(unittest.TestCase):
+    def test_emotion_detector(self):
+
+        result_1 = emotion_detector("I am glad this happened")
+        self.assertEqual(result_1[DOMINANT], "joy")
+
+        result_2 = emotion_detector("I am really mad about this")
+        self.assertEqual(result_2[DOMINANT], "anger")
+
+        result_3 = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(result_3[DOMINANT], "disgust")
+
+        result_4 = emotion_detector("I am so sad about this")
+        self.assertEqual(result_4[DOMINANT], "sadness")
+
+        result_5 = emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(result_5[DOMINANT], "fear")
+
+
+unittest.main()
